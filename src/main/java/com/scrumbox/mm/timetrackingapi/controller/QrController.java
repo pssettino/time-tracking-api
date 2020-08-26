ddf39ddbcb8ce0d9cb7c45509fbe15e5475552b2
Pablo Settino <pablo.settino@mercadolibre.com>
2020-08-26 00:37:53 -0300

    Generate QR code

--- src/main/java/com/scrumbox/mm/timetrackingapi/controller/QrController.java
+++ src/main/java/com/scrumbox/mm/timetrackingapi/controller/QrController.java
@@ -2,8 +2,11 @@ package com.scrumbox.mm.timetrackingapi.controller;
 
 
 import com.scrumbox.mm.timetrackingapi.persistence.domain.Tracking;
+import com.scrumbox.mm.timetrackingapi.service.QrService;
 import javassist.bytecode.ByteArray;
 import net.glxn.qrgen.javase.QRCode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
@@ -12,11 +15,10 @@ import java.io.File;
 @RequestMapping("/api/tracking/qr")
 public class QrController {
 
-    //TODO
-    private final String URL_TRACKING = "localhost:8080/time-tracking-api/api/tracking/";
+    @Autowired
+    private QrService qrService;
 
-    @GetMapping("/{dni}")
-    public File trackingQr(@PathVariable Integer dni) {return QRCode.from(URL_TRACKING + dni).file(dni.toString());
-    }
+    @GetMapping(value = "/{dni}", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public @ResponseBody byte[] trackingQr(@PathVariable String dni) {return qrService.trackingQr(dni); }
 
 }
--- src/main/java/com/scrumbox/mm/timetrackingapi/service/QrService.java
+++ src/main/java/com/scrumbox/mm/timetrackingapi/service/QrService.java
@@ -1,24 +1,39 @@
 package com.scrumbox.mm.timetrackingapi.service;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.scrumbox.mm.timetrackingapi.exception.TimeTrackingException;
+import javassist.bytecode.ByteArray;
+import net.glxn.qrgen.core.image.ImageType;
 import net.glxn.qrgen.javase.QRCode;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 
+@CacheConfig(cacheNames = {"qr"})
 @Service
 public class QrService {
 
-    public static BufferedImage generateQRCodeImage(String barcodeText) throws Exception {
+    private final String TRACKING_API = "http://%s:%s/time-tracking-api/api/tracking/%s";
+
+    @Cacheable
+    public byte[] trackingQr(String dni) {
+        String serviceUrl = String.format(TRACKING_API, "localhost", "8080", dni);
+
         ByteArrayOutputStream stream = QRCode
-                .from(barcodeText)
+                .from(serviceUrl)
+                .to(ImageType.PNG)
                 .withSize(250, 250)
                 .stream();
-        ByteArrayInputStream bis = new ByteArrayInputStream(stream.toByteArray());
 
-        return ImageIO.read(bis);
+        return stream.toByteArray();
     }
-
 }
